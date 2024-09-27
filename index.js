9e373679614321ce10416244883d73a154204642
machgiahao <haomgse184349@fpt.edu.vn>
2024-09-27 16:45:05 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.use(cors()); 
+app.use(cookieParser());
+app.use(express.json());
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
+});
