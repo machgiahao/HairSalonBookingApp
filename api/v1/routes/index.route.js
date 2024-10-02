5f451462027e989a6f1fea537bdd7d5f011e40a0
tantttse <156907341+tantttse@users.noreply.github.com>
2024-10-02 14:33:33 +0700

    reconfig the staff route in index

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -1,10 +1,11 @@
 const authRoute = require("./auth/auth.route")
+const staffRouter = require("./staff/staff.route");
 
 module.exports = (app) => {
     app.use("/api/v1/auth", authRoute);
 
     //STAFF ROUTE
-    app.use("/api/v1/staff",(req,res)=>{res.send('staff')});
+    app.use("/api/v1/staff",staffRouter);
 
     //STYLIST ROUTE
     app.use("/api/v1/stylist",(req,res)=>{res.send('stylist')});
