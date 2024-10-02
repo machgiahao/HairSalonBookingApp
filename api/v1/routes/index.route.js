6a4a7abcccdf9d0e16307e24693162f6d2a163f3
tantttse <156907341+tantttse@users.noreply.github.com>
2024-10-02 14:05:54 +0700

    update index route with staff route + stylist route

--- api/v1/routes/index.route.js
+++ api/v1/routes/index.route.js
@@ -3,6 +3,11 @@ const authRoute = require("./auth/auth.route")
 module.exports = (app) => {
     app.use("/api/v1/auth", authRoute);
 
+    //STAFF ROUTE
+    app.use("/api/v1/staff",(req,res)=>{res.send('staff')});
+
+    //STYLIST ROUTE
+    app.use("/api/v1/stylist",(req,res)=>{res.send('stylist')});
     
 }
 
