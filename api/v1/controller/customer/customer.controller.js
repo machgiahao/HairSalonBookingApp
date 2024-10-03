c33edcb26bbae91d16df41d24f322ae24bc0be91
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-03 22:04:10 +0700

    Add function update customer

--- api/v1/controller/customer/customer.controller.js
+++ api/v1/controller/customer/customer.controller.js
@@ -5,14 +5,14 @@ const customerController = {
         try {
             const id = req.params.id;
             console.log(id)
-            if(!id) {
+            if (!id) {
                 return res.status(400).json({
                     success: "fail",
                     msg: "ID is required"
                 })
             }
             const customer = await baseModel.findById("Customer", "CustomerID", id);
-            if(!customer) {
+            if (!customer) {
                 return res.status(400).json({
                     success: "fail",
                     msg: "Customer not found"
@@ -29,7 +29,39 @@ const customerController = {
                 msg: "Internal server error"
             })
         }
- }
+    },
+
+    update: async (req, res) => {
+        try {
+            const id = req.params.id;
+
+            if(!id) {
+                return res.status(400).json({
+                    success: "fail",
+                    msg: "ID is required"
+                })
+            }
+            
+            const customer = {
+                FullName: req.body.fullName
+            }        
+            const update = await baseModel.update("Customer", "CustomerID", id, Object.keys(customer), Object.values(customer));
+            if (!update) {
+                return res.status(404).json({ error: 'Staff member not found' });
+            }
+            res.status(200).json({
+                success: "true",
+                msg: "Update successfully"
+            })
+        } catch (error) {
+            return res.status(500).json({
+                success: "fail",
+                msg: "Internal server error"
+            })
+        }
+    }, 
+
+
 }
 
 
--- api/v1/routes/customer/customer.route.js
+++ api/v1/routes/customer/customer.route.js
@@ -2,6 +2,7 @@ const customerController = require("../../controller/customer/customer.controlle
 const router = require("express").Router();
 
 router.get("/detail/:id", customerController.detail)
+router.patch("/update/:id", customerController.update)
 
 
 module.exports = router;
