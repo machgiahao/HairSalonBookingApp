5268ab0cc4b7fdaa28f1207e1eea4b3e7e37faa4
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-07 22:07:37 +0700

    Add getCurrent

--- api/v1/routes/user.route.js
+++ api/v1/routes/user.route.js
@@ -1,7 +1,9 @@
 const userController = require("../controller/user.controller")
 const router = require("express").Router();
+const verifyToken = require("../middleware/verifyToken.middleware")
 
-router.get("/getAll", userController.getAll);
+router.get("/getCurrent", verifyToken, userController.getCurrent);
+router.get("/getAll", verifyToken , userController.getAll);
 
 module.exports = router;
 
