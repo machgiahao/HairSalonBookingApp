597fc52f5b9d69358b1c876f90b433f2d8db0b66
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-10 00:33:17 +0700

    Add forgot and reset password using mailer

--- api/v1/controller/auth.controller.js
+++ api/v1/controller/auth.controller.js
@@ -3,6 +3,7 @@ const baseModel = require("../../../model/base.model");
 const validate = require("../../../validates/validateInput");
 const generate = require("../../../helper/generate.helper")
 const roleHelper = require("../../../helper/role.helper");
+const mail = require("../../../helper/sendMails.helper")
 const jwt = require("jsonwebtoken")
 
 const authController = {
@@ -27,7 +28,7 @@ const authController = {
                 phoneNumber: req.body.phoneNumber,
             }
             const user = await baseModel.create("Users", Object.keys(newUser), Object.values(newUser));
-            const userByRole = await roleHelper.handleRole(user, req.body);        
+            const userByRole = await roleHelper.handleRole(user, req.body);
             return res.status(200).json({
                 success: true,
                 data: {
@@ -48,23 +49,25 @@ const authController = {
         try {
             const phoneNumber = req.body.phoneNumber;
 
-            const  validatePhoneError = validate.validateInputField(phoneNumber);
-            if(validatePhoneError) {
+            const validatePhoneError = validate.validateInputField(phoneNumber);
+            if (validatePhoneError) {
                 return res.status(400).json({
                     success: false,
                     msg: validatePhoneError
                 })
             }
             const user = await baseModel.findByPhone("Users", "phoneNumber", phoneNumber);
-            if(!user) {
+            if (!user) {
                 return res.status(401).json({
                     success: false,
                     msg: "Phone number not registed !"
                 })
             }
 
+            console.log(user)
+            console.log(req.body.password)
             const validPassword = await bcrypt.compare(req.body.password, user.password);
-            if(!validPassword) {
+            if (!validPassword) {
                 return res.status(401).json({
                     success: false,
                     msg: "Incorrect password !"
@@ -84,13 +87,13 @@ const authController = {
             });
             // Get additional information based on user role
             const tableByRole = roleHelper.getTableByRole(user);
-            const actorByRole = await baseModel.findById(tableByRole, "userID", user.userID);  
+            const actorByRole = await baseModel.findById(tableByRole, "userID", user.userID);
 
-            const { password, refreshToken,...others} = user;
+            const { password, refreshToken, ...others } = user;
             return res.status(200).json({
                 success: true,
                 actor: actorByRole,
-                records: {...others, accessToken}
+                records: { ...others, accessToken }
             })
         } catch (error) {
             console.log(error)
@@ -99,8 +102,8 @@ const authController = {
                 msg: "Ịnternal server error"
             })
         }
-    }, 
-    
+    },
+
     logout: async (req, res) => {
         res.clearCookie("refreshToken");
         await baseModel.update("Users", "userID", req.body.userID, ["refreshToken"], [""]);
@@ -113,25 +116,25 @@ const authController = {
     requestRefreshToken: async (req, res) => {
         const cookie = req.cookies;
         // Check refresh token is exist in cookie
-        if(!cookie?.refreshToken) {
+        if (!cookie?.refreshToken) {
             return res.status(401).json({
-                success: false, 
+                success: false,
                 msg: "You're not authenticated!"
             })
         }
         // Check refresh token is valid or not 
         jwt.verify(cookie.refreshToken, process.env.JWT_REFRESH_KEY, async (err, user) => {
-            if(err) {
+            if (err) {
                 return res.status(403).json({
-                    success: false, 
+                    success: false,
                     msg: "Refresh token is not valid!"
                 })
             }
             // Check refresh token matches with refresh token stored in db
             const response = await baseModel.findById("Users", "userID", user.userID);
-            if(cookie.refreshToken !== response.refreshToken) {
+            if (cookie.refreshToken !== response.refreshToken) {
                 return res.status(403).json({
-                    success: false, 
+                    success: false,
                     msg: "Refresh token is not valid!"
                 })
             }
@@ -144,11 +147,97 @@ const authController = {
                 path: "/",
                 sameSite: "strict",
             });
-            return res.status(200).json({ 
+            return res.status(200).json({
                 success: true,
-                accessToken: newAccessToken 
+                accessToken: newAccessToken
             });
         })
+    },
+
+    forgotPassword: async (req, res) => {
+        try {
+            const email = req.body.email;
+
+            const user = await baseModel.findByColumn("Users", "email", email);
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    msg: "Phone number is not exist"
+                })
+            }
+
+            const otp = Math.floor(100000 + Math.random() * 900000).toString(); // Create OTP 6 digits
+            const expiresAt = new Date(Date.now() + 3 * 60 * 1000); // OTP expired after 3m
+
+            const otpTable = {
+                userID: user.userID,
+                otpCode: otp,
+                expiresAt: expiresAt
+            }
+
+            await baseModel.create("OtpRequest", Object.keys(otpTable), Object.values(otpTable));
+            await mail.sendMail(email, "Your OTP Code", `<p>Your OTP code is: <b>${otp}</b></p>`);
+
+            return res.status(200).json({
+                success: true,
+                msg: "OTP code has been sent to your email"
+            })
+
+        } catch (error) {
+            console.error('Error in forgot-password:', error); 
+            return res.status(500).json({
+                success: false,
+                msg: "Internal server error"
+            })
+        }
+    },
+
+    resetPassword: async (req, res) => {
+        try {
+            const { email, otp, newPassword } = req.body;
+
+            const user = await baseModel.findByColumn("Users", "email", email);
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    msg: "Phone number is not exist"
+                })
+            }
+
+            const conditions = [
+                { column: 'userID', value: user.userID, operator: '=' },
+                { column: 'otpCode', value: otp, operator: '=' },
+                { column: 'expiresAt', value: new Date(), operator: '>' },
+                { column: 'used', value: false, operator: '=' }
+            ];
+
+            const otpRequest = await baseModel.findWithConditions('OtpRequest', ['*'], conditions, ['AND']);
+
+            if (!otpRequest) {
+                return res.status(400).json({
+                    success: false,
+                    msg: "OTP code is invalid or expired"
+                })
+            }
+
+            // hash password
+            const salt = await bcrypt.genSalt(10);
+            const hashed = await bcrypt.hash(newPassword, salt);
+
+            await baseModel.update("Users", "userID", user.userID, ["password"], [hashed]);
+            await baseModel.update("OtpRequest", "id", otpRequest.id, ["used"], ["true"]);
+
+            return res.status(200).json({
+                success: true,
+                msg: "Update password successfully"
+            })
+        } catch (error) {
+            console.error('Error in forgot-password:', error); 
+            return res.status(500).json({
+                success: false,
+                msg: "Internal server error"
+            })
+        }
     }
 
 
--- api/v1/routes/auth.route.js
+++ api/v1/routes/auth.route.js
@@ -6,7 +6,8 @@ router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 router.post("/refresh", authController.requestRefreshToken);
-// router.post("/changePassword", authController.changePassword);
+router.post("/forgot-password", authController.forgotPassword);
+router.post("/reset-password", authController.resetPassword);
 
 
 module.exports = router;
