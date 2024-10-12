65f26d44643cec69e1c932335a14dde5b3eb0187
tantttse <156907341+tantttse@users.noreply.github.com>
2024-10-13 00:50:22 +0700

    resovled conflict

--- api/v1/routes/staff.route.js
+++ api/v1/routes/staff.route.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const staffController = require("../controller/staff.controller");
-const uploadCloudMiddleware=require("../middleware/uploadCloud.middleware");
 const route = express.Router();
 const uploadCloudMiddleware= require("../middleware/uploadCloud.middleware");
 const { verifyToken, checkRole } = require("../middleware/verifyToken.middleware")
 
-route.use(verifyToken);
+// route.use(verifyToken);
 //get the detail from staff by send param id
 route.get("/detail",staffController.getStaffDetail);
 
 //update the detail from staff by send param id
-<<<<<<< HEAD
 route.patch("/update", uploadCloudMiddleware, staffController.updateStaff);
-=======
-route.patch("/update",uploadCloudMiddleware,staffController.updateStaff);
->>>>>>> 12454d8f82c753156f3d307102abda7c8b261172
 
 route.use(checkRole("Manager"));
 //get all the staff from database
