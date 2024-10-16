2df49e5f281d47d58c3e60a8593bdb90325ef98d
tantttse <156907341+tantttse@users.noreply.github.com>
2024-10-17 00:19:39 +0700

    convert base to query

--- api/v1/controller/salary.controller.js
+++ api/v1/controller/salary.controller.js
@@ -80,9 +80,8 @@ module.exports.dailySalary = async (req, res) => {
         const dateToUse = requestedDate ? new Date(requestedDate) : new Date();
         
         // Format the date to YYYY-MM-DD 00:00:00 for timestamp without timezone
-        const formattedDate = dateToUse.toISOString().split('T')[0] + " 00:00:00"; // e.g., "2024-10-16 00:00:00"
+        const formattedDate = dateToUse.toISOString().split('T')[0] + " 00:00:00"; 
 
-        // Find salary records that match the date and are not marked as deleted
         let result = await baseModel.findWithConditionsJoin(
             dailySalaryTable.name,
             undefined, // Select all columns
--- model/query.model.js
+++ model/query.model.js
@@ -6,27 +6,27 @@ pool.connect();
 const queryModel = {
     executeTransaction: async (queries) => {
         try {
-            await pool.query('BEGIN'); // Bắt đầu transaction
+            await pool.query('BEGIN'); // Start transaction
 
             let results = [];
 
-            // Thực thi từng truy vấn trong danh sách
+            // Execute each query in the list
             for (let i = 0; i < queries.length; i++) {
                 const { text, values } = queries[i];
-                const result = await executeQuery(text, values ); // Thực hiện truy vấn
-                results.push(result); // Lưu kết quả của từng truy vấn
+                const result = await queryModel.executeQuery(text, values); // Execute query
+                results.push(result.rows); // Save the result of each query
             }
 
-            await pool.query('COMMIT'); // Commit nếu không có lỗi
-            return results; // Trả về kết quả của tất cả truy vấn
+            await pool.query('COMMIT'); // Commit if no errors
+            return results; // Return results of all queries
         } catch (error) {
-            await pool.query('ROLLBACK'); // Rollback nếu có lỗi
-            throw error; // Ném lỗi ra ngoài để controller xử lý
+            await pool.query('ROLLBACK'); // Rollback if there's an error
+            throw error; // Throw error for controller to handle
         } finally {
-            pool.release(); // Giải phóng kết nối
+            pool.release(); // Release connection
         }
     },
-
+    
     create: async (tableName, columns) => {
         try {
             const keyArr = keyUtils.getKeysAsArray(columns);
@@ -36,7 +36,7 @@ const queryModel = {
             return query;
         } catch (error) {
             console.error("Error executing create:", error);
-            throw error
+            throw error; // Throw error for controller to handle
         }
     },
 
@@ -55,7 +55,7 @@ const queryModel = {
             return query;
         } catch (error) {
             console.error("Error executing update:", error);
-            throw new Error(`Update operation failed: ${error.message}`);
+            throw error; 
         }
     },
 
@@ -65,189 +65,163 @@ const queryModel = {
             return query;
         } catch (error) {
             console.error("Error executing delete:", error);
-            throw new Error(`Delete operation failed: ${error.message}`);
+            throw error; 
         }
     },
 
     executeQuery: async (query, values) => {
         try {
-            const result = await pool.query(query, values);
+            const result = await Client.query(query, values);
             return result.rows[0];
         } catch (error) {
             console.error("Error executing delete:", error);
-            throw new Error(`Delete operation failed: ${error.message}`);
+            throw error; 
+            
         }
     },
+    
     find: async (tableName, columns = ["*"], { limit, skip } = {}) => {
         try {
-          const setColumns = columns.join(", ");
-          let query = `SELECT ${setColumns} FROM "${tableName}"`;
-    
-    
-          if (limit && skip !== undefined) {
-            const clause = `
-            LIMIT $${1} 
-            OFFSET $${2}`;
-            query += clause;
-          }
-    
-          // return (await pool.query(query, values)).rows;
-          return query;
-        } catch (error) {
-          console.error("Error executing pagination query:", error);
-          throw new Error(`Pagination operation failed: ${error.message}`);
-        }
-      },
-    
-      countDocuments: async (tableName) => {
-        try {
-          const query = `SELECT COUNT(*) FROM "${tableName}"`;
-          // return rows[0].count;
-          return query;
-        } catch (error) {
-          console.error("Error executing countDocuments:", error);
-          throw new Error(`Count operation failed: ${error.message}`);
-        }
-      },
+            const setColumns = columns.join(", ");
+            let query = `SELECT ${setColumns} FROM "${tableName}"`;
 
-      findWithConditionsJoin:  (
-        tableName,
-        columns = ["*"],
-        conditions = [],
-        logicalOperators = ["AND"],
-        joins = []
-    ) => {
+            if (limit && skip !== undefined) {
+                const clause = `
+                LIMIT $${1} 
+                OFFSET $${2}`;
+                query += clause;
+            }
+            return query;
+        } catch (error) {
+            console.error("Error executing pagination query:", error);
+            throw error; 
+        }
+    },
+    
+    countDocuments: async (tableName) => {
+        try {
+            const query = `SELECT COUNT(*) FROM "${tableName}"`;
+            return query;
+        } catch (error) {
+            console.error("Error executing countDocuments:", error);
+            throw error; 
+        }
+    },
+
+    findWithConditionsJoin: (tableName, columns = ["*"], conditions = [], logicalOperators = ["AND"], joins = []) => {
         try {
             const setColumns = columns.join(", ");
             let query = `SELECT ${setColumns} FROM "${tableName}"`; 
             const whereClauses = [];
-    
+
             // Handle joins
             joins.forEach((join) => {
                 const { table, on, type = "INNER" } = join; // Default to INNER JOIN
                 query += ` ${type} JOIN "${table}" ON ${on}`;
             });
-    
+
             if (conditions.length > 0) {
-                let flag=0;
+                let flag = 0;
                 conditions.forEach((condition, index) => {
                     const { column, operator = "=" } = condition;
-    
-                    
-                        if (operator == "BETWEEN" ) {
-                            // Handle BETWEEN condition
-                            whereClauses.push(`"${column}" BETWEEN $${flag+ 1} AND $${flag+ 2}`);
-                            flag+=2;
-                        } else {
-                            whereClauses.push(`"${column}" ${operator} $${flag+ 1}`);
-                            flag+=1;
-                        }
-    
-                        // Add logical operator if it's not the last condition
-                        if (index < conditions.length - 1) {
-                            whereClauses.push(` ${logicalOperators[index] || "AND"} `);
-                        }
 
+                    if (operator == "BETWEEN") {
+                        // Handle BETWEEN condition
+                        whereClauses.push(`"${column}" BETWEEN $${flag + 1} AND $${flag + 2}`);
+                        flag += 2;
+                    } else {
+                        whereClauses.push(`"${column}" ${operator} $${flag + 1}`);
+                        flag += 1;
+                    }
+
+                    // Add logical operator if it's not the last condition
+                    if (index < conditions.length - 1) {
+                        whereClauses.push(` ${logicalOperators[index] || "AND"} `);
+                    }
                 });
-    
+
                 if (whereClauses.length > 0) {
                     query += ` WHERE ${whereClauses.join("")}`;
                 }
             }
-    
-            
-          //   const result = await pool.query(query, values);
-          //   return result.rows;
-          return query;
+
+            return query;
         } catch (error) {
             console.error("Error executing findWithConditions:", error);
-            throw new Error(`Find with conditions failed: ${error.message}`);
+            throw error; 
         }
     },
     
-    
-      findById:  (tableName, idColumn) => {
+    findById: (tableName, idColumn) => {
         try {
-          const query = `SELECT * FROM "${tableName}" WHERE "${idColumn}" = $1`;
-        //   const result = await pool.query(query, [idValue]);
-          // return result.rows[0];
-          return query;
+            const query = `SELECT * FROM "${tableName}" WHERE "${idColumn}" = $1`;
+            return query;
         } catch (error) {
-          console.error("Error executing findById:", error);
-          throw new Error(`Find by ID operation failed: ${error.message}`);
+            console.error("Error executing findById:", error);
+            throw error;
         }
-      },
-    
-      findByPhone:  (tableName, phoneColumn) => {
+    },
+
+    findByPhone: (tableName, phoneColumn) => {
         try {
-          const query = `SELECT * FROM "${tableName}" WHERE "${phoneColumn}" = $1`;
-          // return result.rows[0];
-          return query;
+            const query = `SELECT * FROM "${tableName}" WHERE "${phoneColumn}" = $1`;
+            return query;
         } catch (error) {
-          console.error("Error executing findByPhone:", error);
-          throw new Error(`Find by phone operation failed: ${error.message}`);
+            console.error("Error executing findByPhone:", error);
+            throw error; 
         }
-      },
-    
-      findByField:  (tableName, columnName) => {
+    },
+
+    findByField: (tableName, columnName) => {
         try {
-          const query = `SELECT * FROM "${tableName}" WHERE "${columnName}" = $1`;
-         // return result.rows[0];
-         return query;
+            const query = `SELECT * FROM "${tableName}" WHERE "${columnName}" = $1`;
+            return query;
         } catch (error) {
-          console.error("Error executing findByPhone:", error);
-          throw new Error(`Find by phone operation failed: ${error.message}`);
+            console.error("Error executing findByField:", error);
+            throw error; 
         }
-      },
-    
-      deleteWithConditions:  (tableName, { conditions = [], logicalOperator = " AND " } = {}) => {
+    },
+
+    deleteWithConditions: (tableName, { conditions = [], logicalOperator = " AND " } = {}) => {
         try {
-          if (conditions.length === 0) return false;
-    
-          let query = `DELETE FROM "${tableName}" WHERE `;
-          const whereClauses = [];
-    
-          conditions.forEach((element, i) => {
-            const { column, operator = "=" } = element;
-            whereClauses.push(`${column} ${operator} $${i + 1}`);
-          });
-    
-          query += whereClauses.join(logicalOperator);
-    
-          //return result.rowCount;
-          return query;
+            if (conditions.length === 0) return false;
+
+            let query = `DELETE FROM "${tableName}" WHERE `;
+            const whereClauses = [];
+
+            conditions.forEach((element, i) => {
+                const { column, operator = "=" } = element;
+                whereClauses.push(`${column} ${operator} $${i + 1}`);
+            });
+
+            query += whereClauses.join(logicalOperator);
+            return query;
         } catch (error) {
-          console.error("Error executing delete:", error);
-          throw new Error(`Delete operation failed: ${error.message}`);
+            console.error("Error executing delete:", error);
+            throw error; 
         }
-      },
-    
-      findAllWithPhone:  (roleTable) => {
+    },
+
+    findAllWithPhone: (roleTable) => {
         try {
-          const query = `
-              SELECT 
-                r.*,  -- Lấy tất cả các cột của bảng Customer
-                u."phoneNumber"  -- Lấy cột phoneNumber của bảng Users
-              FROM 
-                "${roleTable}" r
-              JOIN 
-                "Users" u 
-              ON 
-                r."userID" = u."userID";
+            const query = `
+                SELECT 
+                    r.*,  -- Lấy tất cả các cột của bảng Customer
+                    u."phoneNumber"  -- Lấy cột phoneNumber của bảng Users
+                FROM 
+                    "${roleTable}" r
+                JOIN 
+                    "Users" u 
+                ON 
+                    r."userID" = u."userID";
             `;
-    
-          // return result.rows;
-          return query;
+
+            return query;
         } catch (err) {
-          console.error("Error executing delete:", error);
-          throw new Error(`Delete operation failed: ${error.message}`);
+            console.error("Error executing findAllWithPhone:", err);
+            throw error; 
         }
-      }, 
-      
+    },
 }
+
 module.exports = queryModel;
-
-
-  
-  
-  
