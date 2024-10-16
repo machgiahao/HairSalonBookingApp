84f65172b28a8c8b1bab93a5f25b7064978790ad
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-16 23:23:57 +0700

    Update queryModelV2

--- model/query.model.js
+++ model/query.model.js
@@ -4,6 +4,29 @@ const { Client } = require("pg");
 pool.connect();
 
 const queryModel = {
+    executeTransaction: async (queries) => {
+        try {
+            await pool.query('BEGIN'); // Bắt đầu transaction
+
+            let results = [];
+
+            // Thực thi từng truy vấn trong danh sách
+            for (let i = 0; i < queries.length; i++) {
+                const { text, values } = queries[i];
+                const result = await queryModel.executeQuery(text, values ); // Thực hiện truy vấn
+                results.push(result.rows); // Lưu kết quả của từng truy vấn
+            }
+
+            await pool.query('COMMIT'); // Commit nếu không có lỗi
+            return results; // Trả về kết quả của tất cả truy vấn
+        } catch (error) {
+            await pool.query('ROLLBACK'); // Rollback nếu có lỗi
+            throw error; // Ném lỗi ra ngoài để controller xử lý
+        } finally {
+            pool.release(); // Giải phóng kết nối
+        }
+    },
+    
     create: async (tableName, columns) => {
         try {
             const keyArr = keyUtils.getKeysAsArray(columns);
