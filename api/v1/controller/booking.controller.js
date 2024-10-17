6fbf2e5c04eeea467b9bf9ab1aae31a1ab706496
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-17 16:26:38 +0700

    Add rollback to create booking

--- api/v1/controller/booking.controller.js
+++ api/v1/controller/booking.controller.js
@@ -1,4 +1,4 @@
-const baseModel = require("../../../model/base.model");
+const queryModel = require("../../../model/query.model");
 const bookingTable = require("../../../model/table/booking.table");
 const detailTable = require("../../../model/table/bookingDetail.table")
 const { getColsVals } = require("../../../helper/getColsVals.helper");
@@ -6,21 +6,24 @@ const { getColsVals } = require("../../../helper/getColsVals.helper");
 const bookingController = {
     create: async (req, res) => {
         try {
-            req.body.status = req.body.status ?? "Confirmed";
-            const { columns, values } = getColsVals(bookingTable, req.body);
+            const result = await queryModel.executeTransaction(async (client) => {
+                req.body.status = req.body.status ?? "Confirmed";
+                const { columns, values } = getColsVals(bookingTable, req.body);
+                // Create query to create booking
+                const newBooking = await queryModel.create(bookingTable.name, columns, values, client);
+                // Reassign to let bookingDetail can get
+                req.body.bookingID = newBooking.bookingID;
+                const { columns: columnsDetail, values: valuesDetail } = getColsVals(detailTable, req.body);
+                // Create query to create detail
+                const newDetail = await queryModel.create(detailTable.name, columnsDetail, valuesDetail, client);
 
-            // Create booking
-            const newBooking = await baseModel.create(bookingTable.name, columns, values);
+                return {newBooking: newBooking, newDetail: newDetail}
+            });
 
-            // Reassign to let bookingDetail can get
-            req.body.bookingID = newBooking.bookingID;
-            const { columns: columnsDetail, values: valuesDetail } = getColsVals(detailTable, req.body);
-            // Create detail
-            const newDetail = await baseModel.create(detailTable.name, columnsDetail, valuesDetail);
             return res.status(200).json({
                 success: true,
-                newBooking: newBooking,
-                newDetail: newDetail
+                newBooking: result.newBooking,
+                newDetail: result.newDetail
             })
         } catch (error) {
             console.log(error)
@@ -29,7 +32,7 @@ const bookingController = {
                 msg: "Internal server error"
             })
         }
-    }, 
+    },
 
     detail: async (req, res) => {
         const id = req.query.id;
--- model/query.model.js
+++ model/query.model.js
@@ -1,82 +1,72 @@
 const keyUtils = require("../helper/keyUtils.helper");
 const pool = require("../config/connection");
-const { Client } = require("pg");
-pool.connect();
 
 const queryModel = {
-    executeTransaction: async (queries) => {
-        try {
-            await pool.query('BEGIN'); // Bắt đầu transaction
-
-            let results = [];
-
-            // Thực thi từng truy vấn trong danh sách
-            for (let i = 0; i < queries.length; i++) {
-                const { text, values } = queries[i];
-                const result = await executeQuery(text, values ); // Thực hiện truy vấn
-                results.push(result); // Lưu kết quả của từng truy vấn
-            }
-
-            await pool.query('COMMIT'); // Commit nếu không có lỗi
-            return results; // Trả về kết quả của tất cả truy vấn
-        } catch (error) {
-            await pool.query('ROLLBACK'); // Rollback nếu có lỗi
-            throw error; // Ném lỗi ra ngoài để controller xử lý
-        } finally {
-            pool.release(); // Giải phóng kết nối
-        }
-    },
-
-    create: async (tableName, columns) => {
-        try {
-            const keyArr = keyUtils.getKeysAsArray(columns);
-            const setColumns = keyArr.join(", ");
-            const placeholders = values.map((_, i) => `$${i + 1}`).join(", ");
-            const query = `INSERT INTO "${tableName}" (${setColumns}) VALUES (${placeholders}) RETURNING *`;
-            return query;
-        } catch (error) {
-            console.error("Error executing create:", error);
-            throw new Error(`Create operation failed: ${error.message}`);
-        }
-    },
-
-    update: async (tableName, idColumn, columns) => {
-        try {
-            // Check if columns are populated
-            if (columns.length === 0) {
-                throw new Error("No columns provided for the update operation.");
-            }
-
-            const setClause = columns
-                .map((col, i) => `"${col}" = $${i + 1}`) // safely create SET clause
-                .join(", ");
-
-            const query = `UPDATE "${tableName}" SET ${setClause} WHERE "${idColumn}" = $${columns.length + 1} RETURNING *`;
-            return query;
-        } catch (error) {
-            console.error("Error executing update:", error);
-            throw new Error(`Update operation failed: ${error.message}`);
-        }
-    },
-
-    deleteById: async (tableName, idColumn) => {
-        try {
-            const query = `DELETE FROM "${tableName}" WHERE "${idColumn}" = $1`;
-            return query;
-        } catch (error) {
-            console.error("Error executing delete:", error);
-            throw new Error(`Delete operation failed: ${error.message}`);
-        }
-    },
-
-    executeQuery: async (query, values) => {
-        try {
-            const result = await pool.query(query, values);
-            return result.rows[0];
-        } catch (error) {
-            console.error("Error executing delete:", error);
-            throw new Error(`Delete operation failed: ${error.message}`);
-        }
+  executeTransaction: async (transactionCallback) => {
+    const client = await pool.connect(); // Lấy một client từ pool
+    try {
+      await client.query('BEGIN'); // Bắt đầu transaction
+      const result = await transactionCallback(client); // Truyền client vào callback để thực hiện các query
+      await client.query('COMMIT'); // Commit transaction nếu thành công
+      return result;
+    } catch (error) {
+      await client.query('ROLLBACK'); // Rollback nếu có lỗi xảy ra
+      throw error;
+    } finally {
+      client.release(); // Đảm bảo giải phóng client sau khi sử dụng
     }
+  },
+
+  create: async (tableName, columns, values, client) => {
+    try {
+      const keyArr = keyUtils.getKeysAsArray(columns);
+      const setColumns = keyArr.join(", ");
+      const placeholders = values.map((_, i) => `$${i + 1}`).join(", ");
+      const query = `INSERT INTO "${tableName}" (${setColumns}) VALUES (${placeholders}) RETURNING *`;
+      const result = await client.query(query, values);
+      return result.rows.length > 0 ? result.rows[0] : null;
+    } catch (error) {
+      console.error("Error executing create:", error);
+      throw new Error(`Create operation failed: ${error.message}`);
+    }
+  },
+
+  update: async (tableName, idColumn, idValue, columns, values, client) => {
+    try {
+      // Check if columns are populated
+      if (columns.length === 0) {
+        throw new Error("No columns provided for the update operation.");
+      }
+
+      const setClause = columns
+        .map((col, i) => `"${col}" = $${i + 1}`) // safely create SET clause
+        .join(", ");
+
+      const query = `UPDATE "${tableName}" SET ${setClause} WHERE "${idColumn}" = $${columns.length + 1} RETURNING *`;
+
+      // // Log the query and values for debugging
+      // console.log("Generated query:", query);
+      // console.log("Values:", [...values, idValue]);
+
+      // Execute the query
+      const result = await client.query(query, [...values, idValue]);
+
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error executing update:", error);
+      throw new Error(`Update operation failed: ${error.message}`);
+    }
+  },
+
+  deleteById: async (tableName, idColumn, idValue, client) => {
+    try {
+      const query = `DELETE FROM "${tableName}" WHERE "${idColumn}" = $1`;
+      await client.query(query, [idValue]);
+      return true;
+    } catch (error) {
+      console.error("Error executing delete:", error);
+      throw new Error(`Delete operation failed: ${error.message}`);
+    }
+  },
 }
 module.exports = queryModel;
