eaedeeacd903a788c1ca096ea14792e88f254e38
tantttse <156907341+tantttse@users.noreply.github.com>
2024-10-17 16:42:17 +0700

    recover query + add date helper

--- helper/dateRefactor.helper.js
+++ helper/dateRefactor.helper.js
@@ -4,8 +4,15 @@ const dateRefactor = {
         const converted = date.toISOString().split('T')[0] + " 00:00:00";
         return converted;
     },
-    rangeDate: (range)=>{
+    rangeDate: (date)=>{
         const dateToUse = date ? new Date(date) : new Date();
+        const firstDay = new Date(dateToUse.getFullYear(),dateToUse.getMonth(),1);
+        const lastDay = new Date(dateToUse.getFullYear(),dateToUse.getMonth(),0);
+
+        return {
+            firstDay:firstDay.toISOString().split('T')[0] + " 00:00:00",
+            lastDay:lastDay.toISOString().split('T')[0] + " 00:00:00",
+        }
 
     }
 }
