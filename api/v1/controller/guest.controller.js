6d040e30e1f4add1721b55a0802782676f888e78
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-28 22:03:07 +0700

    Add detail, delete, getAll guest

--- api/v1/controller/guest.controller.js
+++ api/v1/controller/guest.controller.js
@@ -1,14 +1,17 @@
 const baseModel = require("../../../model/base.model");
 const guestTable = require("../../../model/table/guest.table");
-const { getColsVals } = require("../../../helper/getColsVals.helper"); 
+const { getColsVals } = require("../../../helper/getColsVals.helper");
 
 const guestController = {
     create: async (req, res) => {
         try {
             const result = await baseModel.executeTransaction(async () => {
-                const {columns, values} = getColsVals(guestTable, req.body);
+                const { columns, values } = getColsVals(guestTable, req.body);
                 const guest = await baseModel.create(guestTable.name, columns, values);
-                return {guest: guest};
+                if (!guest) {
+                    throw new Error("Cannot create")
+                }
+                return { guest: guest };
             })
             return res.status(200).json({
                 success: true,
@@ -17,12 +20,88 @@ const guestController = {
         } catch (error) {
             return res.status(500).json({
                 success: false,
-                msg: "Internal server error"
+                error: error.message
             })
         }
+    },
 
+    detail: async (req, res) => {
+        try {
+            const id = req.query.id;
 
+            const guest = await baseModel.findByField(guestTable.name, guestTable.columns.guestID, id);
+            if (!guest) {
+                throw new Error("Guest not found")
+            }
+
+            return res.status(200).json({
+                success: true,
+                data: guest
+            })
+
+        } catch (error) {
+            return res.status(500).json({
+                success: false,
+                error: error.message
+            })
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const id = req.query.id;
+
+            const result = await baseModel.executeTransaction(async () => {
+                const deleted = await baseModel.update(guestTable.name, guestTable.columns.guestID, id, ["deleted"], [true]);
+                if (!deleted) {
+                    throw new Error("Guest not found");
+                }
+                return deleted
+            })
+
+            res.status(200).json({
+                success: true,
+                msg: "Delete successfully",
+                data: result
+            })
+
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({
+                success: false,
+                error: error.message
+            })
+        }
+    },
+
+    getAll: async (req, res) => {
+        try {
+            const limit = Math.abs(parseInt(req.query.perpage)) || null;
+            const offset = (Math.abs(parseInt(req.query.page) || 1) - 1) * limit;
+
+            const guests = await baseModel.findWithConditions(
+                guestTable.name,
+                undefined,
+                [],
+                [],
+                [],
+                limit,
+                offset
+            )
+
+            if (!guests || guests.length === 0) {
+                return handleError(res, 404, new Error("Guest not found"));
+            }
+
+            return res.status(200).json({
+                success: true,
+                guests: guests
+            })
+        } catch (error) {
+            return handleError(res, 500, error);
+        }
     }
+
 }
 
 module.exports = guestController;
--- api/v1/routes/guest.route.js
+++ api/v1/routes/guest.route.js
@@ -1,6 +1,8 @@
 const guestController = require("../controller/guest.controller");
 const route = require('express').Router();
 
+route.get("/detail", guestController.detail);
 route.post("/create", guestController.create);
+route.delete("/delete", guestController.delete);
 
 module.exports = route;
