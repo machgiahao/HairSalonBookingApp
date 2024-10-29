8d38601b8dbea9da53c68b17d8cbcfac7f46f251
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-29 03:54:35 -0400

    Add handle response and error of guestController

--- api/v1/controller/feedback.controller.js
+++ api/v1/controller/feedback.controller.js
@@ -2,29 +2,33 @@ const feedbackTable = require("../../../model/table/feedback.table");
 const { getColsVals } = require("../../../helper/getColsVals.helper");
 const baseModel = require("../../../model/base.model")
 const handleError = require("../../../helper/handleError.helper");
+const handleResponse = require("../../../helper/handleReponse.helper");
 const bookingTable = require("../../../model/table/booking.table");
 const customerTable = require("../../../model/table/customer.table");
 
 const feedbackController = {
     detail: async (req, res) => {
+        let statusCode
         try {
             const feedbackID = req.query.id;
-
+            if (!isValidId(feedbackID)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const feedback = await baseModel.findByField(feedbackTable.name, feedbackTable.columns.feedbackID, feedbackID);
             if (!feedback) {
-                return handleError(res, 404, new Error("Feedback not found"));
+                statusCode = 404
+                throw new Error("Feedback not found");
             }
-            return res.status(200).json({
-                success: true,
-                feedback: feedback
-            })
 
+            return handleResponse(res, 200, { feedback: feedback })
         } catch (error) {
-            return handleError(res, 500, error);
+            return handleError(res, statusCode, error);
         }
     },
 
     getAll: async (req, res) => {
+        let statusCode
         try {
             const limit = Math.abs(parseInt(req.query.perpage)) || null;
             const offset = (Math.abs(parseInt(req.query.page) || 1) - 1) * limit;
@@ -40,23 +44,26 @@ const feedbackController = {
             )
 
             if (!feedbacks || feedbacks.length === 0) {
-                return handleError(res, 404, new Error("Feedback not found"));
+                statusCode = 404
+                throw new Error("No records of feedback");
             }
 
-            return res.status(200).json({
-                success: true,
-                feedbacks: feedbacks
-            })
+            return handleResponse(res, 200, { feedbacks: feedbacks })
         } catch (error) {
-            return handleError(res, 500, error);
+            return handleError(res, statusCode, error);
         }
     },
 
     create: async (req, res) => {
+        let statusCode
         try {
             const result = await baseModel.executeTransaction(async () => {
                 const { columns: feedbackColumns, values: feedbackValues } = getColsVals(feedbackTable, req.body);
                 const feedback = await baseModel.create(feedbackTable.name, feedbackColumns, feedbackValues);
+                if (!feedback) {
+                    statusCode = 400
+                    throw new Error("Create feedback fail");
+                }
                 const booking = await baseModel.findByField(bookingTable.name, bookingTable.columns.bookingID, req.body.bookingID);
                 let customer = null;
                 if (booking.customerID) {
@@ -65,68 +72,62 @@ const feedbackController = {
                 return { feedback: feedback, customer: customer }
             })
 
-
-            return res.status(200).json({
-                success: true,
+            return handleResponse(res, 200, {
                 data: {
                     feedback: result.feedback,
                     customer: result.customer
                 }
             })
+
         } catch (error) {
-            return handleError(res, 500, error);
+            return handleError(res, statusCode, error);
         }
     },
 
     update: async (req, res) => {
+        let statusCode
         try {
             const feedbackID = req.query.id;
+            if (!isValidId(feedbackID)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const result = await baseModel.executeTransaction(async () => {
                 const { columns: feedbackColumns, values: feedbackValues } = getColsVals(feedbackTable, req.body);
                 const updateFeedback = await baseModel.update(feedbackTable.name, feedbackTable.columns.feedbackID, feedbackID, feedbackColumns, feedbackValues);
                 if (!updateFeedback) {
-                    throw new Error("Feedback not found");
-                    
+                    statusCode = 400
+                    throw new Error("Update feedback fail");
                 }
                 return { updateFeedback: updateFeedback }
             })
 
-            return res.status(200).json({
-                success: true,
-                data: result.updateFeedback
-            })
+            return handleResponse(res, 200, { data: result.updateFeedback })
         } catch (error) {
-            return handleError(res, 500, error);
+            return handleError(res, statusCode, error);
         }
     },
 
     delete: async (req, res) => {
+        let statusCode
         try {
             const feedbackID = req.query.id;
-
+            if (!isValidId(feedbackID)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const result = await baseModel.executeTransaction(async () => {
                 const deleted = await baseModel.update(feedbackTable.name, feedbackTable.columns.feedbackID, feedbackID, ["deleted"], [true]);
                 if (!deleted) {
-                    return res.status(404).json({
-                        success: false,
-                        msg: "Delete fail"
-                    });
+                    statusCode = 400
+                    throw new Error("Delete feedback fail");
                 }
                 return deleted
             })
 
-            res.status(200).json({
-                success: true,
-                msg: "Delete successfully",
-                data: result
-            })
-
+            return handleResponse(res, 200, { data: result })
         } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                success: false,
-                msg: "Internal server error"
-            })
+            return handleError(res, statusCode, error);
         }
     }
 
