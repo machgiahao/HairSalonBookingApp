2bb95a52127df920364494218c3470f2de8564cf
machgiahao <haomgse184349@fpt.edu.vn>
2024-10-29 14:25:33 +0700

    Add handle response and error for customerController

--- api/v1/controller/customer.controller.js
+++ api/v1/controller/customer.controller.js
@@ -1,130 +1,117 @@
 const { getColsVals } = require("../../../helper/getColsVals.helper");
+const handleResponse = require("../../../helper/handleReponse.helper");
 const baseModel = require("../../../model/base.model")
 const customerTable = require("../../../model/table/customer.table")
 const userTable = require("../../../model/table/user.table")
+const isValidId = require("../../../validates/reqIdParam.validate");
 
 const customerController = {
     detail: async (req, res) => {
+        let statusCode
         try {
             const id = req.query.id;
-
+            if (!isValidId(id)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const customer = await baseModel.findByField(customerTable.name, customerTable.columns.customerID, id);
             if (!customer) {
+                statusCode = 404
                 throw new Error("Customer not found");
-                
             }
 
             const user = await baseModel.findByField(userTable.name, userTable.columns.userID, customer.userID);
             if (!user) {
+                statusCode = 404
                 throw new Error("User not found");
-                
+
             }
             const { password, refreshToken, ...others } = user;
-            return res.status(200).json({
-                success: true,
+            return handleResponse(res, 200, {
                 data: {
                     customer: customer,
                     user: others
                 }
             })
-
         } catch (error) {
-            return res.status(500).json({
-                success: false,
-                error: error.message
-            })
+            return handlError(res, statusCode, error);
         }
     },
     update: async (req, res) => {
+        let statusCode
         try {
             const id = req.query.id;
-
+            if (!isValidId(id)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const result = await baseModel.executeTransaction(async () => {
                 const { columns: customerColumns, values: customerValues } = getColsVals(customerTable, req.body);
                 const { columns: userColumns, values: userValues } = getColsVals(userTable, req.body);
                 // Update table customer
                 const updateCustomer = await baseModel.update(customerTable.name, customerTable.columns.customerID, id, customerColumns, customerValues);
                 if (!updateCustomer) {
-                    throw new Error("Customer not found");               
+                    statusCode = 404
+                    throw new Error("Customer not found");
                 }
                 // Update table user
                 const userId = req.body.userID;
                 const updateUser = await baseModel.update(userTable.name, userTable.columns.userID, userId, userColumns, userValues);
                 if (!updateUser) {
-                    return res.status(404).json({ error: 'User not found' });
+                    statusCode = 404
+                    throw new Error("User not found");
                 }
                 const { refreshToken, password, ...others } = updateUser;
                 return { updateCustomer: updateCustomer, updateUser: others }
             })
 
-            res.status(200).json({
-                success: true,
-                msg: "Update successfully",
+            return handleResponse(res, 200, {
                 data: {
                     updateCustomer: result.updateCustomer,
                     updateUser: result.updateUser
                 }
             })
         } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                success: false,
-                msg: "Internal server error"
-            })
+            return handlError(res, statusCode, error);
         }
     },
 
     delete: async (req, res) => {
+        let statusCode
         try {
             const id = req.query.id;
-
+            if (!isValidId(id)) {
+                statusCode = 400
+                throw new Error("Invalid ID");
+            }
             const result = await baseModel.executeTransaction(async () => {
                 const deleted = await baseModel.update(customerTable.name, customerTable.columns.customerID, id, ["deleted"], [true]);
                 if (!deleted) {
-                    return res.status(404).json({
-                        success: false,
-                        msg: "Delete fail"
-                    });
+                    statusCode = 404
+                    throw new Error("Delete customer fail");
                 }
                 return deleted
             })
 
-            res.status(200).json({
-                success: true,
-                msg: "Delete successfully",
-                data: result
-            })
-
+            return handleResponse(res, 200, { data: result })
         } catch (error) {
-            console.log(error)
-            return res.status(500).json({
-                success: false,
-                msg: "Internal server error"
-            })
+            return handlError(res, statusCode, error);
         }
     },
 
     getAll: async (req, res) => {
+        let statusCode
         try {
             const customerList = await baseModel.findAllWithPhone("Customer");
 
             if (!customerList || customerList.length === 0) {
-                return res.status(404).json({
-                    success: false,
-                    msg: 'No customer found'
-                });
+                statusCode = 404
+                throw new Error("No records of customer");
             }
 
-            res.status(200).json({
-                success: true,
-                customerList: customerList
-            })
-
+            return handleResponse(res, 200, { customerList: customerList })
         } catch (error) {
-            return res.status(500).json({
-                success: false,
-                msg: "Internal server error"
-            })
+            return handlError(res, statusCode, error);
         }
     }
 
