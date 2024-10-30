6b4340b307da58b2763c31f0c0cad2d5ea5ac494
Sang1011 <153055469+Sang1011@users.noreply.github.com>
2024-10-31 01:27:18 +0700

    update routes

--- api/v1/routes/payment.route.js
+++ api/v1/routes/payment.route.js
@@ -7,15 +7,16 @@ route.get("/getAll", paymentController.getAll);
 route.use(verifyToken);
 route.get("/getDetail", paymentController.getDetail);
 
-// route.use(checkRole("Stylist"));
+route.use(checkRole("Stylist"));
 route.post("/create", paymentController.create);
 
-// route.use(checkRole("Manager", "Staff"));
-route.post("/generateQR", paymentController.generateQR);
-route.patch("/update", paymentController.update);
-route.delete("/softDel", paymentController.softDel);
 route.post("/create_payment_url", express.urlencoded({ extended: true }), verifyToken, checkRole("Staff"), paymentController.createPaymentUrl);
 route.get("/return_url", verifyToken, checkRole("Staff"), paymentController.returnUrl);
 route.get("/vnpay_ipn", verifyToken, checkRole("Staff"), paymentController.vnpayIpn);
+
+route.use(checkRole("Manager", "Staff"));
+route.post("/generateQR", paymentController.generateQR);
+route.patch("/update", paymentController.update);
+route.delete("/softDel", paymentController.softDel);
 
 module.exports = route;
